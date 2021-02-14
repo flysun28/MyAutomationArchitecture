d1050a93ad1ae93cbdfd9fe5711f7b26678156db
80319739 <yanfei1@oppo.com>
2021-02-14 21:27:19 +0800

    python_standard modify import path

--- lib/pb_src/python_standard/AutoUpdate_pb2.py
+++ lib/pb_src/python_standard/AutoUpdate_pb2.py
@@ -13,7 +13,7 @@ from google.protobuf import descriptor_pb2
 _sym_db = _symbol_database.Default()
 
 
-from pb_src.python_standard import BaseHeaderOut_pb2
+from lib.pb_src.python_standard import BaseHeaderOut_pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
