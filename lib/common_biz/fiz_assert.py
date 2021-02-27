7e6e67f2d6fc764a397fac406641312d8d7ce6f3
80264408 <xiaoyao1@oppo.com>
2021-02-27 21:38:14 +0800

    fiz_assert改为unittest断言方法

--- lib/common_biz/fiz_assert.py
+++ lib/common_biz/fiz_assert.py
@@ -10,17 +10,18 @@ from lib.common.utils.globals import GlobarVar
 from lib.common.utils.meta import WithLogger
 from lib.common_biz.find_database_table import SeparateDbTable
 from lib.config.path import common_sql_path
-
+import unittest
 logger = Logger('FizAssert').get_logger()
 
 
-class FizAssert(metaclass=WithLogger):
+class FizAssert(unittest.TestCase, metaclass=WithLogger):
     def __init__(self, in_out="inland"):
         self.in_out = in_out
         self.mysql = GlobarVar.MYSQL_IN
         self.mysql_out = GlobarVar.MYSQL_OUT
+        self._type_equality_funcs = {}
 
-    def assert_order_info(self, ssoid, pay_req_id, amount, original_amount, kb_spent=0 or None, vou_amount=0 or None, vou_id=None):
+    def assert_order_info(self, ssoid, pay_req_id, amount, original_amount, kb_spent=0, vou_amount=0, vou_id=None):
         db_order_info = SeparateDbTable(ssoid).get_order_db_table()
         order_info = {}
         sql_oder = str(Config(common_sql_path).read_config("order", "order")).format(
@@ -31,11 +32,11 @@ class FizAssert(metaclass=WithLogger):
             order_info = self.mysql_out.select_one(sql_oder)
         self.logger.info("订单详情：{}".format(order_info))
         try:
-            assert order_info is not None
-            assert order_info['amount'] == amount
-            assert order_info['original_amount'] == original_amount
-            assert order_info['status'] == "OK"
-            assert order_info['kebi_spent'] == kb_spent
+            self.assertIsNotNone(order_info)
+            self.assertEqual(order_info['amount'], amount)
+            self.assertEqual(order_info['original_amount'], original_amount)
+            self.assertEqual(order_info['status'], "OK")
+            self.assertEqual(order_info['kebi_spent'], kb_spent)
             assert order_info['voucher_amount'] == vou_amount
             # 海外与国内订单表不一致，海外无该字段
             if self.in_out == "inland":
@@ -52,12 +53,12 @@ class FizAssert(metaclass=WithLogger):
                 db_trade_order_info[0], db_trade_order_info[1], ssoid, pay_req_id))
         self.logger.info("trade订单详情：{}".format(trade_order_info))
         try:
-            assert trade_order_info is not None
-            assert trade_order_info['amount'] == amount
-            assert trade_order_info['origin_amount'] == original_amount
-            assert trade_order_info['status'] == "OK"
-            assert trade_order_info['kb_amount'] == kb_amount
-            assert trade_order_info['voucher_amount'] == vou_amount
+            self.assertIsNotNone(trade_order_info)
+            self.assertEqual(trade_order_info['amount'], amount)
+            self.assertEqual(trade_order_info['origin_amount'], original_amount)
+            self.assertEqual(trade_order_info['status'], "OK")
+            self.assertEqual(trade_order_info['kb_amount'], kb_amount)
+            self.assertEqual(trade_order_info['voucher_amount'], vou_amount)
             self.logger.info("trade订单表信息记录正确")
         except AssertionError as e:
             self.logger.info("trade订单表信息记录异常")
@@ -81,11 +82,11 @@ class FizAssert(metaclass=WithLogger):
         self.logger.info("tb_recharge订单详情：{}".format(recharge_info))
         try:
             if flag:
-                assert recharge_info is not None
-                assert recharge_info['amount'] == amount
-                assert recharge_info['status'] == "OK"
+                self.assertIsNotNone(recharge_info)
+                self.assertEqual(recharge_info['amount'], amount)
+                self.assertEqual(recharge_info['status'], "OK")
             else:
-                assert recharge_info is None
+                self.assertIsNone(recharge_info)
             self.logger.info("tb_recharge订单表信息记录正确")
         except AssertionError as e:
             self.logger.info("tb_recharge订单表信息记录异常")
@@ -109,11 +110,11 @@ class FizAssert(metaclass=WithLogger):
         self.logger.info("tb_payment订单详情：{}".format(payments_info))
         try:
             if flag:
-                assert payments_info is not None
-                assert payments_info['price'] == amount
-                assert payments_info['status'] == "OK"
+                self.assertIsNotNone(payments_info)
+                self.assertEqual(payments_info['price'], amount)
+                self.assertEqual(payments_info['status'], "OK")
             else:
-                assert payments_info is None
+                self.assertIsNone(payments_info)
             self.logger.info("tb_payment订单表信息记录正确")
         except AssertionError as e:
             self.logger.info("tb_payment订单表信息记录正确")
@@ -132,10 +133,10 @@ class FizAssert(metaclass=WithLogger):
             notify_info = self.mysql_out.select_one(sql_notify)
         self.logger.info("通知表信息详情：{}".format(notify_info))
         try:
-            assert notify_info is not None
-            assert notify_info['notify_response'] == "OK" or "ABANDON"
+            self.assertIsNotNone(notify_info)
+            self.assertIn(notify_info['notify_response'], "OK", "ABANDON")
             if notify_info['notify_response'] == "OK":
-                assert notify_info['notify_count'] == 1
+                self.assertEqual(notify_info['notify_count'], 1)
             self.logger.info("通知成功")
         except AssertionError as e:
             self.logger.info("通知异常")
@@ -154,8 +155,8 @@ class FizAssert(metaclass=WithLogger):
             str(Config(common_sql_path).read_config("order", "sign_status")).format(ssoid, partner_code,
                                                                                     renew_product_code, pay_type))
         try:
-            assert sign_status is not None
-            assert sign_status['status'] == "SIGN"
+            self.assertIsNotNone(sign_status)
+            self.assertEqual(sign_status['status'], "SIGN")
             self.logger.info("签约信息表记录正确")
         except AssertionError as e:
             self.logger.info("签约信息表记录异常")
@@ -169,8 +170,8 @@ class FizAssert(metaclass=WithLogger):
         sign_record_status = self.mysql.select_one(
             str(Config(common_sql_path).read_config("order", "sign_record_status")).format(pay_req_id))
         try:
-            assert sign_record_status is not None
-            assert sign_record_status['status'] == "SUCC"
+            self.assertIsNotNone(sign_record_status)
+            self.assertEqual(sign_record_status['status'], "SUCC")
             self.logger.info("签约记录表记录正确")
         except Exception as e:
             self.logger.info("签约记录表记录异常")
@@ -190,8 +191,8 @@ class FizAssert(metaclass=WithLogger):
             vou_info = self.mysql_out.select_one(sql_vou)
         self.logger.info("优惠券详情：{}".format(vou_info))
         try:
-            assert vou_info is not None
-            assert vou_info['status'] == status
+            self.assertIsNotNone(vou_info)
+            self.assertEqual(vou_info['status'], status)
             self.logger.info("优惠券表记录正确")
         except Exception as e:
             self.logger.info("优惠券表记录异常")
