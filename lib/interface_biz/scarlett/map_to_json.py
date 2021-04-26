fb08ce9e37b95d629302ee2c3404202a22af5a41
80319739 <yanfei1@oppo.com>
2021-04-26 12:25:56 +0800

    scarlett/map_to_json.py

--- lib/interface_biz/scarlett/map_to_json.py
+++ lib/interface_biz/scarlett/map_to_json.py
@@ -4,20 +4,31 @@
 # datetime:2021/4/25 13:22
 # comment:
 import json
+import re
 
-a = "{charset=UTF-8, notify_time=2021-04-25 13:21:38, unsign_time=2021-04-25 13:21:38, alipay_user_id=2088112811111403, sign=IQzBbJZcuH9T5AgsbV+1Cefyppne3K8AmPMMxfv2vwVCdt94hMG2K40qeNT4bkv84yJbSXjtwdiM9PNK0yyVUvRXH3/fuRdGTzzmexGf+sdZAqskavvCYud0JrOp4U7+SGzD0002lirXm+QeQarLw5Ppu7NkXdgQRhjFb85l4MtcZpxIay7JqHXC3IK6S2IwcZ1EIvk1cZTrfR7e8CtpfY+YGLf/HzZ1HbT6xAZcEl54fp0uM9rXOgXfejbOul/9eS1b8P5VEFPmQ/UR1oqEJEuWo4N4qwWpqYbkqO49aM7aonYP9muOHtS+EWCG1BMeEq2WXekJHy0+2cO3Z2nfig==, external_agreement_no=SN202104251121441327164428238554, version=1.0, notify_id=2021042500222132138017361420359661, notify_type=dut_user_unsign, agreement_no=20215225715271617440, auth_app_id=2016120904060189, personal_product_code=GENERAL_WITHHOLDING_P, app_id=2016120904060189, sign_type=RSA2, alipay_logon_id=156******06, status=UNSIGN, sign_scene=INDUSTRY|GAME_CHARGE}"
 
-
-def scarlet_map_to_json(req):
+def _scarlet_map_to_json(req):
     b = req.replace("=", '"="')
     c = b.replace(", ", '","')
     e = c.replace("{", '{"')
     f = e.replace("}", '"}')
     g = f.replace("=", ":")
     h = g.replace('":"":"",', '==", ')
-    # print(eval(h), type(eval(h)))
     return eval(h)
 
 
+def scarlet_map_to_json(req):
+    imatches = re.finditer('(.*?)(,|})\s*', req[1:])
+    for m in imatches:
+        pair = m.group(1)
+        left, _, right = pair.partition('=')
+        left = '"' + left.strip() + '"'
+        right = '"' + right.strip() + '"'
+        repl = ''.join([left, ': ', right])
+        req = req.replace(pair, repl)
+    return req
+
+
 if __name__ == '__main__':
+    a = '{charset=UTF-8, notify_time=2021-04-26 11:26:35, alipay_user_id=2088202596648570, sign=NiiXCW16BvKX9hxixPlxqxhAAU+Rj9LaUU26TZTmE+VUFo+pHGEAxE1ubfjBBiGL7+rh9mtK3n+If1jyeOE9XNeQWeHNXihJYUjLnbnWhf57e7kpKvJW+wjZLqt+xKlmYnpWBcKyzjn7lTR8EWlkim/MpOyBAQU5rhNDZPGzN9AWx8pI9FTO/cK2gw+Wq8J1SDD9vX/4mn1f2rjIozDmiTrmKc+Q9ir/gHQ2ows9YP3Remx/IdlKsa7lzaUWXQo8o3Zh8fNKvnZG0EnEbSkaO7JZCrIIz0/OKp6/1tf9Ks7JYkwmsKcGi0MfoZ2Ej/9pIJNeOhUJcEvTbauym+uTAw==, external_agreement_no=SN202104261125516785511073180072, version=1.0, sign_time=2021-04-26 11:26:35, notify_id=2021042600222112635013531415389971, notify_type=dut_user_sign, agreement_no=20215226712418230557, auth_app_id=2016120904060189, invalid_time=2115-02-01 00:00:00, personal_product_code=GENERAL_WITHHOLDING_P, valid_time=2021-04-26 11:26:35, app_id=2016120904060189, sign_type=RSA2, alipay_logon_id=280***@qq.com, status=NORMAL, sign_scene=INDUSTRY|GAME_CHARGE}'
     print(scarlet_map_to_json(a))
